5b0a6d6db0dcb87c8c1dfbce22ce11336f52e357
lichengtao <lichengtao@ipaychat.com>
2017-07-06 15:36:26 +0800

    revise

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,12 +1,67 @@
 <template>
 <div class="goods">
-  <div class="menu-wrapper"></div>
-  <div class="foods-wrapper"></div>
+  <div class="menu-wrapper">
+    <ul>
+      <li v-for="(item,index) in goods" class="menu-item">
+       <span class="text border-1px">
+         <span class="icon" v-show="item.type>0" :class="classMap[item.type]"></span>{{item.name}}
+       </span>
+      </li>
+    </ul>
+  </div>
+  <div class="foods-wrapper">
+    <ul>
+      <li v-for="(item,index) in goods" class="food-list">
+        <h1 class="title">{{item.name}}</h1>
+        <ul>
+          <li v-for="(food,index) in item.foods" class="food-item border-1px">
+            <div class="icon">
+              <img :src="food.icon" width="57" height="57">
+            </div>
+            <div class="content">
+              <h2 class="name">{{food.name}}</h2>
+              <p class="desc">{{food.description}}</p>
+              <div class="extra">
+                <span class="count">月售{{food.sellCount}}份</span>
+                <span>好评率{{food.rating}}</span>
+              </div>
+              <div class="price">
+                <span   class="now">￥{{food.price}}</span>
+                <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 
 <script>
-export default {};
+const ERR_OK=0;
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      goods: []
+    };
+  },
+  created() {
+    this.classMap=['decrease','discount','special','invoice','guarantee'];
+    this.$http.get('/api/goods/').then((response) => {
+      response=response.body;
+      if(response.errno===ERR_OK){
+        this.goods=response.data;
+        console.log(this.goods)
+      }
+    });
+  }
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
